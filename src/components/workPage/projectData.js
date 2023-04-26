0eeb2893d95885fdd63623c561a40813a4066441
Fouji5131 <mabdullahr5131@gmail.com>
2023-04-26 17:31:56 +0500

    todo project added

--- src/components/workPage/projectData.js
+++ src/components/workPage/projectData.js
@@ -1,15 +1,18 @@
 import ProjectImage from "../../images/projectImg.png";
+import TodoList from "../../images/Daily ToDo List.PNG";
 
 const ProjectsData = [
   {
-    title: "UX case study",
-    projectName: "Bally Website Research",
-    image: ProjectImage,
+    title: "Practice Project",
+    projectName: "Daily To-Do List",
+    image: TodoList,
+    projectUrl: "https://daily-to-do-list.vercel.app/",
   },
   {
     title: "UX study",
     projectName: "Website Research",
     image: ProjectImage,
+    projectUrl: "https://www.google.com/",
   },
   // {
   //   title: "Graphic Design",
--- src/components/workPage/projectItem.js
+++ src/components/workPage/projectItem.js
@@ -3,10 +3,17 @@ import ProjectImg from "../../images/projectImg.png";
 import ArrowBlack from "../../images/arrowBlack.png";
 
 const ProjectItem = (props) => {
+  // function newTab() {
+  //   window.open(props.projectsData.projectUrl);
+  // }
+
   return (
     <div className=" xl:space-y-0 xl:m-10  grid grid-cols-1 xl:grid-cols-2 gap-12 2xl:gap-20 space-y-0 ">
       {props.projectsData.map((projects) => (
-        <div className="flex flex-col xl:flex-row space-y-5 xl:space-y-0 rounded-3xl shadow-2xl p-10 zoomUp">
+        <div
+          className="flex flex-col xl:flex-row space-y-5 xl:space-y-0 rounded-3xl shadow-2xl p-10 zoomUp"
+          // onClick={newTab}
+        >
           <div className="xl:w-1/2 space-y-1 2xl:space-y-5 ">
             <p className="font-boldItalic text-sm sm:text-md 2xl:text-lg">
               {projects.title}
@@ -17,11 +24,14 @@ const ProjectItem = (props) => {
             {/* <img src={ArrowBlack} alt="" /> */}
           </div>
           <div className="flex justify-center object-contain xl:w-1/2 ">
-            <img
-              className="rounded-3xl shadow-2xl"
-              src={projects.image}
-              alt=""
-            />
+            <a href={projects.projectUrl} target="blank">
+              <img
+                className="rounded-3xl shadow-2xl"
+                src={projects.image}
+                alt=""
+                // onClick={projects.projectUrl}
+              />
+            </a>
           </div>
         </div>
       ))}
